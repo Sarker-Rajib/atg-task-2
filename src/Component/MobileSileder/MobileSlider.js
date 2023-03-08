b0104dee5ff13fca9276e9088b4e635dfec8306c
Rajib Sarker <redhat0593@gmail.com>
2023-03-08 14:23:37 +0600

    lets start with mobile screen

---
+++ src/Component/MobileSileder/MobileSlider.js
@@ -0,0 +1,39 @@
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination } from "swiper";
+
+const MobileSlider = () => {
+    return (
+        <>
+            <Swiper
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={true}
+                modules={[Autoplay, Pagination]}
+                className="mySwiper">
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default MobileSlider;
